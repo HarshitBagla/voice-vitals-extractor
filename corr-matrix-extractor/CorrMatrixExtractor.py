e48bb712e9544cdc45f1bf26d2f99db5dc0871db
Harshit Bagla <hbagla2@illinois.edu>
2022-04-22 21:38:15 -0500

    Shifted the smile extractor to corrMat

--- corr-matrix-extractor/CorrMatrixExtractor.py
+++ corr-matrix-extractor/CorrMatrixExtractor.py
@@ -6,9 +6,11 @@ import logging
 import os
 from pyclowder.extractors import Extractor
 import pyclowder.files
+import opensmile
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 
 
 class CorrMatrixExtractor(Extractor):
@@ -34,10 +36,45 @@ class CorrMatrixExtractor(Extractor):
 
         logger = logging.getLogger(__name__)
         inputfile = resource["local_paths"][0]
+        file_id = resource['id']
         dataset_id = resource['parent'].get('id')
 
         # These process messages will appear in the Clowder UI under Extractions.
         connector.message_process(resource, "Loading contents of file...")
+        
+        # Call actual program
+        # Execute word count command on the input file and obtain the output
+        smile = opensmile.Smile(
+            feature_set=opensmile.FeatureSet.ComParE_2016,
+            feature_level=opensmile.FeatureLevel.Functionals,
+        )
+
+        # 1. Create metadata dictionary
+        y = smile.process_file(inputfile)
+
+        m = y.to_dict('records')[0]
+        result = {
+            'audspec_lengthL1norm_sma_range': m['audspec_lengthL1norm_sma_range'],
+            'audspec_lengthL1norm_sma_maxPos': m['audspec_lengthL1norm_sma_maxPos'],
+            'audspec_lengthL1norm_sma_minPos': m['audspec_lengthL1norm_sma_minPos']
+        }
+        # connector.message_process(resource, "Found %s lines and %s words..." % (lines, words))
+
+        # Store results as metadata
+        metadata = self.get_metadata(result, 'file', file_id, host)
+
+        # Normal logs will appear in the extractor log, but NOT in the Clowder UI.
+        logger.debug(metadata)
+
+        # Upload metadata to original file
+        pyclowder.files.upload_metadata(connector, host, secret_key, file_id, metadata)
+
+        # 2. store table as new file and upload
+        original_filename = resource["name"]
+        filename = os.path.splitext(original_filename)[0] + "_summary.csv"
+        y.to_csv(filename, index=False)
+        dataset_id = resource['parent'].get('id')
+        pyclowder.files.upload_to_dataset(connector, host, secret_key, dataset_id, filename)
         
         # Making the corr Matrix after every file upload
         files_in_dataset = pyclowder.datasets.get_file_list(connector, host, secret_key, dataset_id)
